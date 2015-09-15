9bee2175f8b4778320f8c295fd62a3592f051237
Gustavo Carvalho <gustavo.bio@gmail.com>
2015-09-15 18:21:18 -0300

    Inicial commit. Function application.
    App is still a bit rough on the edges and needs a few tweaks here and
    there, but is fully functional.

---
+++ app.R
@@ -0,0 +1,271 @@
+library(shiny)
+library(flora)
+library(tpl)
+library(shinydashboard)
+library(DT)
+
+ui <- dashboardPage(skin = "blue",
+  dashboardHeader(title = "Plantminer"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Select a database:"),
+      menuItem("Brazilian Flora Checklist", tabName = "flora", icon = icon("leaf")),
+      menuItem("The Plant List", tabName = "tpl", icon = icon("leaf")),
+      menuItem("Source code on Github", icon = icon("file-code-o"),
+               href = "http://github.com/gustavobio/plantminer/"),
+      sidebarUserPanel("Gustavo Carvalho", image = "https://avatars2.githubusercontent.com/u/30267?v=3&s=460", subtitle = tags$a("Profile on Github", href = "http://www.github.com/gustavobio"))
+    )
+  ),
+  dashboardBody(
+    tabItems(
+      tabItem(
+        ## Brazilian Flora UI
+        tabName = "flora",
+        h4("Brazilian Flora Checklist - ", tags$a("Flora R package", href = "http://github.com/gustavobio/flora"), "frontend"),
+        fluidRow(
+          column(width = 3,
+                 box(title = "1. Name options", width = NULL, collapsible = TRUE,
+                     checkboxInput("synonyms", label = "Replace synonyms", value = TRUE),
+                     checkboxInput("suggest", label = "Correct misspelled names", value = TRUE)),
+                 box(title = "2. Tweak suggestions", width = NULL, collapsible = TRUE, collapsed = TRUE, 
+                     p("How conservative the name guessing should be? Lower values are less conservative and may result in incorrect suggestions."),
+                     sliderInput("distance", label = "", 
+                                 min = 0.7, max = 1, value = 0.9, animate = TRUE)), 
+                 box(title = "3. Life form & more", width = NULL, collapsible = TRUE, collapsed = TRUE,
+                     p("Checking these boxes will generate duplicated rows if a taxon has multiple habitats, vernacular names, and so on."),
+                     checkboxInput("life.form", label = "Life form", value = FALSE),
+                     checkboxInput("habitat", label = "Habitat", value = FALSE),
+                     checkboxInput("states", label = "Occurrence", value = FALSE),
+                     checkboxInput("vernacular", label = "Show common names", value = FALSE),
+                     checkboxInput("establishment", label = "Establishment", value = FALSE)),
+                 box(title = "4. Paste your taxa", width = NULL, solidHeader = TRUE, collapsible = TRUE,
+                     p("One taxon per line. Taxa can be families, genera, or full names, down to varieties and subspecies. Don't include authorities. Longer lists usually take a few minutes to process."),
+                     tags$form(
+                       tags$textarea(id = "taxa", rows= 8 , cols = 21, "Miconia albicans\nMyrcia lingua\nCofea arabica\nFabaceae\nMusa\nTabebuia sp.1"),
+                       tags$br(),
+                       submitButton(text = "Process list")))),
+          column(width = 9,
+                 box(title = "5. Processed taxa", width = NULL,
+                     p("Columns might be automatically removed from display to fit the width of your screen.
+                        IDs are links to taxa on the", tags$a("Brazilian Flora website", href = "http://floradobrasil.jbrj.gov.br/jabot/listaBrasil/PrincipalUC/PrincipalUC.do"), 
+                       ", which is the source of all data used here. Please cite them accordingly.
+                       Threat statuses are determined by", 
+                       tags$a("CNC Flora", href = "http://cncflora.jbrj.gov.br"), "and follow the IUCN convention."),
+                     DT::dataTableOutput(outputId="contents")),
+                 box(title = "6. Download", width = NULL, collapsible = TRUE, collapsed = FALSE,
+                     p("The files linked below have columns separated by commas, semicolons, or tabulations. 
+                       All of them can be opened in virtually any spreadsheet and statistical software. 
+                       The phylomatic taxa file is used with", tags$a("Phylomatic", href = "http://phylodiversity.net/phylocom/"), "to generate a phylogenetic tree. 
+                       Taxa Plantminer couldn't match against the database are ommited from this file, but not from the others."),
+                     # fluidRow(
+                     #  valueBoxOutput("stats.found", width = 2),
+                     #  valueBoxOutput("stats.notfound", width = 2),
+                     #  valueBoxOutput("stats.synonyms", width = 2),
+                     #  valueBoxOutput("stats.misspeled", width = 2),
+                     #  valueBoxOutput("stats.total", width = 1)
+                     # ),
+                       downloadButton('downloadDataCsv', 'Comma-delimited'),
+                       downloadButton('downloadDataCsv2', 'Semicolon-delimited'),
+                       downloadButton('downloadDataTab', 'Tab-delimited'),
+                       downloadButton('downloadDataPhylomatic', 'Phylomatic taxa')
+                     )
+                 )
+        )),
+      ## The Plant List UI
+      tabItem(
+        tabName = "tpl",
+        h4("The Plant List - ", tags$a("TPL R package", href = "http://github.com/gustavobio/tpl"), "frontend"),
+        fluidRow(
+          column(width = 3,
+                 box(title = "1. Name options", width = NULL, collapsible = TRUE,
+                     checkboxInput("synonyms.tpl", label = "Replace synonyms", value = TRUE),
+                     checkboxInput("apg", label = "Return APG families", value = FALSE),
+                     checkboxInput("get.synonyms.tpl", label = "Return synonyms of all taxa", value = FALSE),
+                     checkboxInput("suggest.tpl", label = "Correct misspelled names", value = TRUE),
+                     p("How conservative the name guessing should be? Lower values are less conservative and may result in incorrect suggestions."),
+                     sliderInput("distance.tpl", label = "", 
+                                 min = 0.7, max = 1, value = 0.9, animate = TRUE)), 
+                 box(title = "2. Paste your species", width = NULL, solidHeader = TRUE, collapsible = TRUE,
+                     p("One full name (genus and species) per line. Don't include authorities. Longer lists usually take a few minutes to process."),
+                     tags$form(
+                       tags$textarea(id = "taxa.tpl", rows= 8 , cols = 21, "Miconia albicans\nMyrcia lingua\nCofea arabica\nFabaceae\nMusa\nTabebuia sp.1"),
+                       tags$br(),
+                       submitButton(text = "Process list")))),
+          column(width = 9,
+                 box(title = "3. Processed taxa", width = NULL,
+                     p("Columns might be automatically removed from display to fit the width of your screen.
+                       IDs are links to taxa on ", tags$a("The Plant List", href = "http://www.theplantlist.org"), ", which is the source of all data used here (v1.1). 
+                       Please cite them accordingly. Follow", tags$a("this link", href = "http://www.theplantlist.org/1.1/about/"), "for further details on the data."),
+                     DT::dataTableOutput(outputId="contents.tpl")),
+                 box(title = "4. Download", width = NULL, collapsible = TRUE, collapsed = FALSE,
+                     p("The files linked below have columns separated by commas, semicolons, or tabulations.
+                       All of them can be opened in virtually any spreadsheet and statistical software. 
+                       The phylomatic taxa file is used with", tags$a("Phylomatic", href = "http://phylodiversity.net/phylocom/"), "to generate a phylogenetic tree. 
+                       Taxa Plantminer couldn't match against the database are ommited from this file, but not from the others."),
+                    # fluidRow(
+                    #   valueBoxOutput("stats.found.tpl", width = 2),
+                    #   valueBoxOutput("stats.notfound.tpl", width = 2),
+                    #   valueBoxOutput("stats.synonyms.tpl", width = 2),
+                    #   valueBoxOutput("stats.misspeled.tpl", width = 2),
+                    #   valueBoxOutput("stats.total.tpl", width = 1)
+                    # ),
+                     downloadButton('downloadDataCsvTpl', 'Comma-delimited'),
+                     downloadButton('downloadDataCsv2Tpl', 'Semicolon-delimited'),
+                     downloadButton('downloadDataTabTpl', 'Tab-delimited'),
+                     downloadButton('downloadDataPhylomaticTpl', 'Phylomatic taxa')
+                 )
+          )
+        )
+      )
+    )
+  )
+)
+server <- function(input, output) {
+  ##
+  ## Brazilian flora server side
+  ##
+    
+  process.taxa <- reactive({
+    user.taxa <- unlist(strsplit(input$taxa, "\n"))
+    processed.list <- get.taxa(user.taxa, 
+                               replace.synonyms = input$synonyms, 
+                               suggest.names = input$suggest, 
+                               life.form = input$life.form, 
+                               habitat = input$habitat, 
+                               vernacular = input$vernacular, 
+                               states = input$states, 
+                               establishment = input$establishment,
+                               suggestion.distance = input$distance)
+    processed.list
+  })
+  output$contents <- DT::renderDataTable({
+    processed.list <- process.taxa()
+    links.flora <- 
+      paste("<a target=\"_blank\" href = \"http://floradobrasil.jbrj.gov.br/jabot/listaBrasil/FichaPublicaTaxonUC/FichaPublicaTaxonUC.do?id=FB", processed.list$id, "\">", processed.list$id,"</a>", sep = "")
+    links.flora <- gsub("FBNA", NA, links.flora)
+    out <- data.frame(id = links.flora, processed.list[, -c(1, 3)])
+    first.columns <- c("id", "scientific.name", "original.search", "search.str", "taxon.status", "notes", "taxon.rank")
+    other.columns <- names(out)[!names(out) %in% first.columns]
+    out <- out[c(first.columns, other.columns)]
+    names(out) <- gsub("\\.", " ", names(out))
+    output$downloadDataCsv <- downloadHandler(
+      filename = "results.csv",
+      content = function(file = filename) {      
+        write.csv(processed.list, file,
+                  row.names = FALSE, quote = TRUE)
+      }
+    )
+    output$downloadDataCsv2 <- downloadHandler(
+      filename = "results.csv",
+      content = function(file = filename) {      
+        write.csv2(processed.list, file,
+                   row.names = FALSE, quote = TRUE)
+      }
+    )
+    output$downloadDataTab <- downloadHandler(
+      filename = "results_tab.txt",
+      content = function(file = filename) {      
+        write.table(processed.list, file,
+                    row.names = FALSE, quote = TRUE)
+      }
+    )
+    output$downloadDataPhylomatic <- downloadHandler(
+      filename = "results_phylomatic.txt",
+      content = function(file = filename) {      
+        cat(df2phytaxa(processed.list), file = file, sep = "\n")
+      }
+    )
+    # Comenting this out until I find a better way to present stats
+    #     output$stats.notfound <- renderValueBox({
+    #       valueBox(sum(grepl("not found", out$notes)), "not found", color = "red")
+    #     })
+    #     output$stats.found <- renderValueBox({
+    #       valueBox(sum(!grepl("not found", out$notes)), "found", color = "olive")
+    #     }) 
+    #     output$stats.synonyms <- renderValueBox({
+    #       valueBox(sum(grepl("replaced synonym", out$notes)), "resolved synonyms", color = "yellow")
+    #     }) 
+    #     output$stats.misspeled <- renderValueBox({
+    #       valueBox(sum(grepl("was misspelled", out$notes)), "corrected names", color = "navy")
+    #     }) 
+    #     output$stats.total <- renderValueBox({
+    #       valueBox(nrow(out), "total", color = "blue")
+    #     }) 
+    out
+  }, extensions = c('Responsive', 'ColReorder'), options = list(dom = 'Rlfrtip'), escape = 1)
+  
+  
+  ##
+  ## The Plant List server side
+  ##
+  
+  
+  process.taxa.tpl <- reactive({
+    user.taxa.tpl <- unlist(strsplit(input$taxa.tpl, "\n"))
+    processed.list.tpl <- tpl.get(user.taxa.tpl, 
+                                  replace.synonyms = input$synonyms.tpl, 
+                                  suggest.names = input$suggest.tpl,
+                                  apg.families = input$apg,
+                                  suggestion.distance = input$distance.tpl,
+                                  return.synonyms = input$get.synonyms.tpl
+    )
+    processed.list.tpl
+  })
+  output$contents.tpl <- DT::renderDataTable({
+    processed.list.tpl <- process.taxa.tpl()
+    if (input$get.synonyms.tpl) {
+      processed.list.tpl <- processed.list.tpl$synonyms
+    }
+    links.tpl <- 
+        paste("<a target=\"_blank\" href = \"http://www.theplantlist.org/tpl1.1/record/", processed.list.tpl$id, "\">", processed.list.tpl$id,"</a>", sep = "")
+    links.tpl <- gsub("\\/NA", NA, links.tpl)
+    out <- data.frame(id = links.tpl, processed.list.tpl[, -c(1, 3)])
+    names(out) <- gsub("\\.", " ", names(out))
+    output$downloadDataCsvTpl <- downloadHandler(
+      filename = "results.csv",
+      content = function(file = filename) {      
+        write.csv(processed.list.tpl, file,
+                  row.names = FALSE, quote = TRUE)
+      }
+    )
+    output$downloadDataCsv2Tpl <- downloadHandler(
+      filename = "results.csv",
+      content = function(file = filename) {      
+        write.csv2(processed.list.tpl, file,
+                   row.names = FALSE, quote = TRUE)
+      }
+    )
+    output$downloadDataTabTpl <- downloadHandler(
+      filename = "results_tab.txt",
+      content = function(file = filename) {      
+        write.table(processed.list.tpl, file,
+                    row.names = FALSE, quote = TRUE)
+      }
+    )
+    output$downloadDataPhylomaticTpl <- downloadHandler(
+      filename = "results_phylomatic.txt",
+      content = function(file = filename) {      
+        cat(df2phylomatic(processed.list.tpl), file = file, sep = "\n")
+      }
+    )
+    # Comenting this out until I find a better way to present stats
+    #     output$stats.notfound.tpl <- renderValueBox({
+    #       valueBox(sum(grepl("not found", out$notes)), "not found", color = "red")
+    #     })
+    #     output$stats.found.tpl <- renderValueBox({
+    #       valueBox(sum(!grepl("not found", out$notes)), "found", color = "olive")
+    #     }) 
+    #     output$stats.synonyms.tpl <- renderValueBox({
+    #       valueBox(sum(grepl("replaced synonym", out$notes)), "resolved synonyms", color = "yellow")
+    #     }) 
+    #     output$stats.misspeled.tpl <- renderValueBox({
+    #       valueBox(sum(grepl("was misspelled", out$notes)), "corrected names", color = "navy")
+    #     }) 
+    #     output$stats.total.tpl <- renderValueBox({
+    #       valueBox(nrow(out), "total", color = "blue")
+    #     }) 
+    out
+  }, extensions = c('Responsive', 'ColReorder'), options = list(dom = 'Rlfrtip'), escape = 1)
+  
+}
+shinyApp(ui, server)
