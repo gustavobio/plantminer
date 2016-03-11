35cf9f5d2fe20ecbd1e911914f3679cb2fb5bcd4
Gustavo Carvalho <gustavo.bio@gmail.com>
2016-03-10 21:25:06 -0300

    Small fixes

--- run_local.R
+++ run_local.R
@@ -1,10 +1,9 @@
-if (!require(devtools)) {
-  message("Installing devtools")
+if (!require('devtools', quietly = TRUE)) {
+  message("Installing devtools...")
   install.packages("devtools", quiet = TRUE)
-  require(devtools)
+  library('devtools')
 }
-
-if (require(flora)) {
+if (require('flora', quietly = TRUE)) {
   if (packageDescription("flora")$Version != "0.2.7") {
     message("Updating flora")
     install_github("gustavobio/flora")
@@ -12,22 +11,29 @@ if (require(flora)) {
 } else {
   message("Installing flora")
   install_github("gustavobio/flora")
+  library('flora')
 }
 
-if (!require(tpl)) {
-  message("Installing tpl and tpldata (this usually takes a few minutes")
+if (!require('tpl', quietly = TRUE)) {
+  message("Installing tpl and tpldata (this usually takes a few minutes)")
   install_github("gustavobio/tpldata")
   install_github("gustavobio/tpl")
 }
 
-if (!require(shiny)) {
+if (!require('shiny', quietly = TRUE)) {
   message("Installing shiny")
   install.packages("shiny", quiet = TRUE)
+  library('shiny')
 }
 
-if (!require(DT)) {
+if (!require('shinydashboard', quietly = TRUE)) {
   message("Installing shinydashboard")
   install.packages("shinydashboard", quiet = TRUE)
+}
+
+if (!require('DT', quietly = TRUE)) {
+  message("Installing DT")
+  install.packages("DT", quiet = TRUE)
 }
 
 download.file("https://raw.githubusercontent.com/gustavobio/plantminer/master/app.R", paste0(tempdir(), "app.R"))
